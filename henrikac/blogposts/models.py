c7b24c259c70a765a866b76b9ef477b2e2f060d0
Henrik Christensen <sensen1695@hotmail.com>
2018-01-23 20:04:46 +0100

    created Comment model to blogposts app

--- henrikac/blogposts/models.py
+++ henrikac/blogposts/models.py
@@ -19,3 +19,17 @@ class BlogPost(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super(BlogPost, self).save(*args, **kwargs)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    comment = models.TextField()
+    author = models.CharField(max_length=40)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return 'Comment from <{}> to <{}>'.format(self.author, self.post.title)

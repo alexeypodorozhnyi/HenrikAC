b8c4c2ab26ca026add43de3e1318442141bba541
Henrik Christensen <sensen1695@hotmail.com>
2018-01-24 23:29:24 +0100

    created SignUpView

--- henrikac/henrikac/settings.py
+++ henrikac/henrikac/settings.py
@@ -139,3 +139,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
+
+# Authentication
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'blog:list'
+LOGOUT_REDIRECT_URL = 'blog:list'
--- henrikac/users/views.py
+++ henrikac/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('blog:list')
+
+    def form_valid(self, form):
+        valid = super().form_valid(form)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password1']
+        new_user = authenticate(self.request, username=username, password=password)
+        login(self.request, new_user)
+        return valid

7417c2d0eaaaf1edc0b73cd5916b4375dd298029
Henrik Christensen <sensen1695@hotmail.com>
2018-01-24 19:08:16 +0100

    updated BlogPostDetailView to handle CommentForm

--- henrikac/blogposts/views.py
+++ henrikac/blogposts/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponseForbidden
+from django.urls import reverse
 from django.views import generic
+from django.views.generic.edit import FormMixin
 
+from . import forms
 from . import models
 
 
@@ -8,5 +11,39 @@ class BlogPostListView(generic.ListView):
     model = models.BlogPost
 
 
-class BlogPostDetailView(generic.DetailView):
+# https://docs.djangoproject.com/en/1.8/topics/class-based-views/mixins/#using-formmixin-with-detailview
+class BlogPostDetailView(FormMixin, generic.DetailView):
     model = models.BlogPost
+    form_class = forms.CommentForm
+
+    def get_success_url(self):
+        return reverse('blog:detail', kwargs={'slug': self.object.slug})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = models.Comment.objects.filter(post=self.object)
+        context['form'] = self.get_form()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden()
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        author = self.request.user.username
+
+        if self.request.user.is_superuser:
+            author = 'Admin'
+
+        models.Comment.objects.create(
+            post=self.object,
+            comment=form.cleaned_data['comment'],
+            author=author,
+        )
+        return super().form_valid(form)

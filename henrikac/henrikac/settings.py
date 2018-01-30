54d1db174ae0bbbed8cc0e6a0a78ff1d4c84931b
Henrik Christensen <sensen1695@hotmail.com>
2018-01-30 17:06:55 +0100

    added django debug toolbar

--- henrikac/henrikac/settings.py
+++ henrikac/henrikac/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = '=c11h5m&wu-t@)8muns(5+5ps-3eo0^0q9wuh%ltq-5b699az%'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+INTERNAL_IPS = ['127.0.0.1']
 
 # Application definition
 
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'bootstrap4',
     'blogposts',
     'portfolioprojects',
@@ -62,6 +63,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
--- henrikac/henrikac/urls.py
+++ henrikac/henrikac/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -25,3 +26,9 @@ urlpatterns = [
     path('', include('users.urls', namespace='users')),
     path('', views.HomeView.as_view(), name='home'),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
